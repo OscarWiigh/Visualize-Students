09cc109c3a04316dbb9a270ec258c2e32e446775
Oscar Wiigh <oscar.wiigh@gmail.com>
2019-01-23 23:01:26 +0100

    Dataset transition works

--- src/components/Graph/index.js
+++ src/components/Graph/index.js
@@ -3,35 +3,53 @@ import "./graph.css";
 import * as d3 from 'd3';
 import * as ReactFauxDOM from 'react-faux-dom'
 
- 
+
 class Graph extends React.Component {
 
   constructor(props) {
     super(props);
     this.drawChart = this.drawChart.bind(this)
 
-    this.state = {
-      data: [],
-      skill: "gsx$howwouldyourateyourcollaborationskills"
-      
-    }
-}
+  }
 
-componentDidMount() {
-  const { data } = this.props;
-  this.drawChart(data)
-}
-componentWillReceiveProps({data}) {
-  this.drawChart(data)
-}
-shouldComponentUpdate() {
-  return false;
-}
+  componentDidMount() {
+    const { data } = this.props;
+    this.drawChart(data)
+  }
+  componentWillReceiveProps({ data }) {
+    this.updateChart(data)
+  }
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  updateChart(dataset) {
+    var diameter = 800;
+    var nodes = d3.hierarchy(dataset)
+      .sum(function (d) { return d.Count; });
+
+      var bubble = d3.pack(dataset)
+      .size([diameter, diameter])
+      .padding(1.5);
+
+    var node = d3.selectAll(".node").data(bubble(nodes).descendants()).filter(function (d) {
+      return !d.children
+    }).transition().attr("transform", function (d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    }).select("circle").attr("r", function (d) {
+      return d.r
+    }).duration(1000);
+
+    node.select("circle").transition().attr("r", function (d) {
+      return d.r;
+    }).duration(200);
+
+    //node.select("#tHarry Potter ").attr("font-size", 15)
+  }
 
   drawChart(dataset) {
-
     d3.selectAll("svg").remove();
-    
+
     var diameter = 800;
 
     var bubble = d3.pack(dataset)
@@ -43,7 +61,6 @@ shouldComponentUpdate() {
       .attr("width", diameter)
       .attr("height", diameter)
       .attr("class", "bubble");
-      
 
     var nodes = d3.hierarchy(dataset)
       .sum(function (d) { return d.Count; });
@@ -60,17 +77,17 @@ shouldComponentUpdate() {
         return "translate(" + d.x + "," + d.y + ")";
       });
 
-    node.append("title")
-      .text(function (d) {
-        return d.data.Name + ": " + d.data.Count;
-      });
-
     node.append("circle")
-      .on("click", function (d) {
-        console.log(d.data.Name)
-      })
       .attr("r", function (d) {
         return d.r;
+      })
+      .on("click", function (d) {
+        if (d3.select(this).style('fill') !== 'orange') {
+          d3.select(this).style("fill", "orange");
+        }
+        else {
+          d3.select(this).style("fill", d.data.Color)
+        }
       })
       .style("fill", function (d) {
         return d.data.Color;
@@ -83,6 +100,7 @@ shouldComponentUpdate() {
       .text(function (d) {
         return d.data.Name.substring(0, d.r / 3);
       })
+      .attr("id", function (d){ return "t" + d.data.Name})
       .attr("font-family", "sans-serif")
       .attr("font-size", function (d) {
         return d.r / 5;
@@ -101,11 +119,12 @@ shouldComponentUpdate() {
       })
       .attr("fill", "white");
   }
- 
-  render () {
+
+  render() {
     return (
       <div id="chart" />
-    );}
+    );
+  }
 }
 
 export default Graph;

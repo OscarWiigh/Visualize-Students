b95cc73ff917a3e596f19a1c8789ad4a0241dac9
Oscar Wiigh <oscar.wiigh@gmail.com>
2019-01-26 15:44:07 +0100

    Updates

---
+++ precache-manifest.f67e9634c31a6692216a59efc00c4c42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dde2b07491a194605e97",
+    "url": "/Visualize-Students/static/js/runtime~main.dde2b074.js"
+  },
+  {
+    "revision": "1efa5ada173fe7e60ffd",
+    "url": "/Visualize-Students/static/js/main.1efa5ada.chunk.js"
+  },
+  {
+    "revision": "5cf2406976deecb0c382",
+    "url": "/Visualize-Students/static/js/1.5cf24069.chunk.js"
+  },
+  {
+    "revision": "1efa5ada173fe7e60ffd",
+    "url": "/Visualize-Students/static/css/main.0add4d1e.chunk.css"
+  },
+  {
+    "revision": "e7fb907053f110677022b75b3a57e786",
+    "url": "/Visualize-Students/index.html"
+  }
+];
